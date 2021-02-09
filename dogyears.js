00a1beb3063464f2b8afa7204b73bbfc55d8d86b
manojpannala <manojpannala@outlook.com>
2021-02-09 23:02:22 +0200

    dogyears

---
+++ dogyears.js
@@ -0,0 +1,22 @@
+// Converting Human Years to Dog Years
+// My Age
+let myAge = 26
+// early years
+let earlyYears = 2
+
+earlyYears = earlyYears * 10.5
+
+// later years
+laterYears = myAge - 2
+// dog years
+laterYears = laterYears * 4
+
+//console.log(earlyYears)
+//console.log(laterYears)
+
+// My Age in Dog Years
+let myAgeInDogYears = (earlyYears + laterYears)
+
+myName = 'Manoj Reddy'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`);
